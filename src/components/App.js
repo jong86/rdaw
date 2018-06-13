f52193356701108bb41d72a74a3fe372e0d9a3cc
Jon Gaspar <jon_gaspar@hotmail.com>
2018-06-12 20:15:05 -0700

    added jss and global styling in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,34 @@
 // @flow
 import React, { Component } from 'react';
+import injectSheet from 'react-jss';
 import DeviceBar from './DeviceBar';
 
-type Props = {};
+const styles = {
+  '@global': {
+    body: {
+      fontFamily: 'Arial',
+      backgroundColor: '#2c3e50',
+    },
+  }
+};
+
+type Props = {
+  classes: Object,
+};
 
 type State = {};
 
 class App extends React.Component<Props, State> {
   render() {
+    const { classes } = this.props;
+
     return (
-      <div>
+      <div className={classes.container}>
         <DeviceBar />
       </div>
     );
   }
-}
+};
 
+App = injectSheet(styles)(App);
 export default App;
--- src/components/DeviceBar.js
+++ src/components/DeviceBar.js
@@ -2,20 +2,29 @@
 import React, { Component } from 'react';
 import injectSheet from 'react-jss';
 
+const styles = {
+  container: {
+    border: '1px solid tomato',
+  }
+}
 
-type Props = {};
+type Props = {
+  classes: Object,
+};
 
 type State = {};
 
 class DeviceBar extends React.Component<Props, State> {
   render() {
+    const { classes } = this.props
+
     return (
-      <div>
+      <div className={classes.container}>
         hello from devicebar
       </div>
     );
   }
 }
 
-DeviceBar = injectSheet(DeviceBar);
+DeviceBar = injectSheet(styles)(DeviceBar);
 export default DeviceBar;
