4c03ca9cab5a6a2da9dd4c94db445f2cb932c886
Jon Gaspar <jon_gaspar@hotmail.com>
2018-09-08 12:23:05 -0700

    comment about web-audio-mock

--- src/music/audioContext.js
+++ src/music/audioContext.js
@@ -1,7 +1,6 @@
+// Having web-audio-mock here prevents errors when running tests
 const wamock = require("web-audio-mock-api");
-
 let audioContext;
-
 if (process.env.NODE_ENV === 'test') {
   audioContext = new wamock.AudioContext()
 } else {
